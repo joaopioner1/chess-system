04e9b57edafe025cee2aae6e8ed3dbcd2d53f9f1
Joao Pioner <joaopioner123@gmail.com>
2021-04-27 09:11:30 -0300

    First commit chess system

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,15 @@
 package application;
 
-public class Program {
+import boardgame.Board;
 
+public class Program {
+	
+	public static void main(String[] args) {
+		//Author: Joao Vitor Souza Pioner | Date: 26/04/2021 10:38
+		//Last edition: 
+		
+		Board board = new Board(6, 6);
+		 
+		
+	}
 }
