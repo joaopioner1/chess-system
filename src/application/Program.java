81b565e4f52601c8c4ae402721239419fc41fbbb
Joao Pioner <joaopioner123@gmail.com>
2021-04-27 10:23:12 -0300

    Placing pieces on the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,7 @@ public class Program {
 	
 	public static void main(String[] args) {
 		//Author: Joao Vitor Souza Pioner | Date: 26/04/2021 10:38
-		//Last edition: 
+		//Last edition: 27/04/21 9:55
 		
 		ChessMatch cm = new ChessMatch();
 		
