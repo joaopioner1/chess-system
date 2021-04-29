d42602e64ae92170e347fd39817935c4c991dc54
Joao Pioner <joaopioner123@gmail.com>
2021-04-29 11:07:35 -0300

    Possible moves for a piece

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -14,4 +14,10 @@ public class Rook extends ChessPiece{
 	public String toString() {
 		return "R"; //Rook = R (torre)
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 }
