7b7af039ec20104b3829ef31710baae53dce8ebc
Joao Pioner <joaopioner123@gmail.com>
2021-05-12 14:31:15 -0300

    promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,7 +13,7 @@ import chess.ChessPosition;
 public class Program {
 
 	public static void main(String[] args) {
-		
+		//finish project in 12/05/2021
 		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
